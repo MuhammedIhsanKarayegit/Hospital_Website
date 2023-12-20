9dc6e5e9a3a1da8207e9205b4c4f3e39e919d2ff
MuhammedIhsanKarayegit <104142526+MuhammedIhsanKarayegit@users.noreply.github.com>
2023-12-20 17:11:35 +0300

    Hospital Website
    Commits

---
+++ WEB-Proje/Hospital-Management/Hospital-Management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hospital_Management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WEB-Proje/Hospital-Management/Hospital.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
