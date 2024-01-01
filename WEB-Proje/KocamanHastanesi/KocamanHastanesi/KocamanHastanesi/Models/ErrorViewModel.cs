ee48d31330b59e96aab5b07491c7f60095e20043
MuhammedIhsanKarayegit <104142526+MuhammedIhsanKarayegit@users.noreply.github.com>
2024-01-01 19:18:18 +0300

    Delete WEB-Proje/KocamanHastanesi/KocamanHastanesi directory

--- WEB-Proje/KocamanHastanesi/KocamanHastanesi/KocamanHastanesi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KocamanHastanesi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
